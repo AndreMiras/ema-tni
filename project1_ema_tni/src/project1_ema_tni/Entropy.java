39ba2d3ef8b47c3d0fa4550de08b64e9a9f9cce8
Andre Miras <andre.miras@gmail.com>
2012-04-19 16:39:17 +0200

    initial commit with project 1 (work in progress)

---
+++ project1_ema_tni/src/project1_ema_tni/Entropy.java
@@ -0,0 +1,101 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project1_ema_tni;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author andre
+ */
+public class Entropy
+{
+    private String text;
+    private HashMap<Character, Integer> alphabetCountDict =
+            new HashMap<Character, Integer>();
+    private HashMap<Character, Float> alphabetProbaDict =
+            new HashMap<Character, Float>();
+
+    public Entropy(String text)
+    {
+        this.text = text;
+        computeAlphabetCountDict();
+    }
+
+    private void computeAlphabetCountDict()
+    {
+        Character symbol;
+        Integer count = 0;
+        for (int i = 0; i < text.length(); i++)
+        {
+            symbol = text.charAt(i);
+            count = alphabetCountDict.get(symbol);
+            if(count == null) // if the doesn't symbol exist in the lookup table
+            {
+                count = 0;
+            }
+            count++;
+            alphabetCountDict.put(symbol, count);
+        }
+    }
+
+    private void computeAlphabetProbaDict()
+    {
+        getAlphabetCountDict(); // generates alphabet count if empty
+        int count;
+        float proba;
+        int total = text.length();
+
+        for (Character symbol : alphabetCountDict.keySet())
+        {
+            count = alphabetCountDict.get(symbol);
+            proba = (float)count / (float)total;
+            alphabetProbaDict.put(symbol, proba);
+        }
+    }
+
+    public float getTextEntropy()
+    {
+        float probaSymbol;
+        float entropy = 0;
+        getAlphabetProbaDict(); // generates alphabet proba if empty
+
+        for(Character symbol : alphabetProbaDict.keySet())
+        {
+            probaSymbol = alphabetProbaDict.get(symbol);
+            entropy -= probaSymbol * Math.log(probaSymbol)/Math.log(2);
+        }
+        return entropy;
+    }
+
+    /**
+     * Generates and returns alphabet count
+     * @return alphabet count
+     */
+    public HashMap<Character, Integer> getAlphabetCountDict()
+    {
+        if (alphabetCountDict.isEmpty())
+        {
+            computeAlphabetCountDict();
+        }
+
+        return alphabetCountDict;
+    }
+
+    /**
+     * Generates and returns alphabet proba
+     * @return alphabet proba
+     */
+    public HashMap<Character, Float> getAlphabetProbaDict()
+    {
+        if (alphabetProbaDict.isEmpty())
+        {
+            computeAlphabetProbaDict();
+        }
+
+        return alphabetProbaDict;
+    }
+}
---
+++ project1_ema_tni/src/project1_ema_tni/Main.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1_ema_tni;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author andre
+ */
+public class Main
+{
+
+    private static String filePath = "";
+    private static String fileName = "exemple1.txt";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        String text = "comment ca va, petit test entropie";
+        String fullPath = filePath + fileName;
+        try
+        {
+            text = readFile(fullPath);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        Entropy entropyObj = new Entropy(text);
+
+        System.out.println("\nCount:");
+        HashMap<Character, Integer> alphabetCountDict =
+                entropyObj.getAlphabetCountDict();
+        printKeysValuesDict(alphabetCountDict);
+
+        System.out.println("\nProba:");
+        HashMap<Character, Float> alphabetProbaDict =
+                entropyObj.getAlphabetProbaDict();
+        printKeysValuesDict(alphabetProbaDict);
+
+        System.out.println("\nEntropy:");
+        float textEntropy = entropyObj.getTextEntropy();
+        System.out.println(textEntropy);
+    }
+
+    private static <K,V> void printKeysValuesDict(
+            HashMap<K, V> alphabetCountDict)
+    {
+        V value;
+        for(K key : alphabetCountDict.keySet())
+        {
+            value = alphabetCountDict.get(key);
+            System.out.println(key + ": " + value);
+        }
+    }
+
+    private static String readFile(String path) throws IOException
+    {
+        FileInputStream stream = new FileInputStream(new File(path));
+        try
+        {
+            FileChannel fc = stream.getChannel();
+            MappedByteBuffer bb = fc.map(
+                    FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            /* Instead of using default, pass in a decoder. */
+            return Charset.defaultCharset().decode(bb).toString();
+        } finally
+        {
+            stream.close();
+        }
+    }
+}
