0704ed1ed7f3a81ce0f9b1edfe1e0b71e7353c27
Andre Miras <andre.miras@gmail.com>
2012-05-28 17:55:24 +0200

    n-gram code refacto

--- project4_ema_tni/python_scripts/steven_script.py
+++ project4_ema_tni/python_scripts/steven_script.py
@@ -19,81 +19,49 @@ if __name__=="__main__":
 
 # trouver l'entropie (nombre de bits par symboles)
 
-selected_encoding = "utf-8"
-# selected_encoding="iso-8859-1"
-f = codecs.open(file_name, "r", encoding=selected_encoding)
-char_count = 0
-
-alphabet = {}
-char = str(f.read(1)) # .lower()
-while char:
-    char2 = f.read(1)
-    chaine = char + char2
-    if chaine not in alphabet:
-        alphabet.update({chaine:0})
-    alphabet[chaine] += 1
-    char_count += 2
-    char = f.read(1) # .lower()
-f.close()
-
-
-# prob digramme
-print("=================================DIGRAMMES=================================")
-probcharinit = 0
-for lettre in alphabet:
-    nb_occ_char = alphabet[lettre]
-    prob_char = nb_occ_char / float(char_count)
-   # print(str(lettre) + ":" + str(prob_char))
-    if prob_char > probcharinit :
-        moreLettre = lettre
-        probcharinit = prob_char
-print(str(moreLettre) + " : " + str(probcharinit))
-
-sorted_alphabet = sorted(alphabet.iteritems(), key=operator.itemgetter(1))
-sorted_alphabet.reverse()
-compteur = 0
-for lettre in sorted_alphabet:
-    print(str(lettre))
-
-
+# selected_encoding = "utf-8"
+selected_encoding="iso-8859-1"
 
 def n_gram(n_gram_size):
     # prob n-gram
     print("=================================" + str(n_gram_size) + "-grammes=================================")
-    f = open(file_name)
+    # f = open(file_name)
+    f = codecs.open(file_name, "r", encoding=selected_encoding)
     char_count = 0
 
-    alphabet2 = {}
-    char = str(f.read(1)) # .lower()
+    alphabet = {}
+    char = f.read(1) # .lower()
     chaine = char
     while char:
         for i in range(1, n_gram_size):
             chaine += f.read(1)
-        if chaine not in alphabet2:
-            alphabet2.update({chaine:0})
-        alphabet2[chaine] += 1
+        if chaine not in alphabet:
+            alphabet.update({chaine:0})
+        alphabet[chaine] += 1
         char_count += 1
         char = f.read(1) # .lower()
         chaine = char
     f.close()
 
+    """
     probcharinit = 0
-    for lettre in alphabet2:
-        nb_occ_char = alphabet2[lettre]
+    for lettre in alphabet:
+        nb_occ_char = alphabet[lettre]
         prob_char = nb_occ_char / float(char_count)
-       # print(str(lettre) + ":" + str(prob_char))
+        print(str(lettre) + ":" + str(prob_char))
         if prob_char > probcharinit :
             moreLettre = lettre
             probcharinit = prob_char
     print(str(moreLettre) + " : " + str(probcharinit))
+    """
 
-    sorted_alphabet2 = sorted(alphabet2.iteritems(), key=operator.itemgetter(1))
+    sorted_alphabet = sorted(alphabet.iteritems(), key=operator.itemgetter(1))
     # sorted_alphabet.reverse()
-    for lettre in sorted_alphabet2:
+    for lettre in sorted_alphabet:
         print(str(lettre))
 
 
-# n_gram(2)
+n_gram(2)
 
     # prob trigrammes
 print("=================================TRIGRAMMES=================================")
