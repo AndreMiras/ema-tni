7e8867ba611cd5b410da778a861d930a8c8fd4a2
Andre Miras <andre.miras@gmail.com>
2012-05-29 19:52:01 +0200

    LzDictionary unit tests work in progress

--- project4_ema_tni/test/project4_ema_tni/LzDictionaryTest.java
+++ project4_ema_tni/test/project4_ema_tni/LzDictionaryTest.java
@@ -4,10 +4,12 @@
  */
 package project4_ema_tni;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.NavigableSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -17,9 +19,21 @@ import static org.junit.Assert.*;
  */
 public class LzDictionaryTest
 {
+    private static String filePath = "";
+    private static String fileName = "exemple1.txt";
+    private static String text;
     
     public LzDictionaryTest()
     {
+        String fullPath = filePath + fileName;
+        try
+        {
+            text = Helper.readFile(fullPath);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            text = "";
+        }
     }
 
     @BeforeClass
@@ -49,12 +63,30 @@ public class LzDictionaryTest
     public void testGetNGramCountDict()
     {
         System.out.println("getNGramCountDict");
-        LzDictionary instance = null;
-        HashMap expResult = null;
-        HashMap result = instance.getNGramCountDict();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        LzDictionary lzDictionary = new LzDictionary(text, 2);
+        int expResult = 551;
+
+        HashMap<String, Integer> alphabetCountDict =
+                lzDictionary.getNGramCountDict();
+        // there are 551 different digrams in the exemple1.txt file
+        assertEquals(expResult, alphabetCountDict.size());
+    }
+
+    /**
+     * Testing few known digrams
+     */
+    @Test
+    public void testDiGrams()
+    {
+        System.out.println("Testing few digrams");
+        LzDictionary lzDictionary = new LzDictionary(text, 2);
+
+        HashMap<String, Integer> alphabetCountDict =
+                lzDictionary.getNGramCountDict();
+
+        assertEquals(18, alphabetCountDict.get("fo").intValue());
+        assertEquals(4, alphabetCountDict.get("ob").intValue());
+        assertEquals(57, alphabetCountDict.get("ar").intValue());
     }
 
     /**
